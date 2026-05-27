bba253671351f6188612b4996576fc4fa3b55bcb
Bauti Dallan <bautidallan2000@gmail.com>
2026-05-27 13:33:29 -0300

    F

--- BackEnd/database.py
+++ BackEnd/database.py
@@ -1,5 +1,4 @@
 import os
-import ssl
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 
@@ -10,8 +9,7 @@ MONGO_URI = os.getenv("MONGO_URI")
 client = AsyncIOMotorClient(
     MONGO_URI,
     tls=True,
-    tlsAllowInvalidCertificates=True,
-    ssl_cert_reqs=ssl.CERT_NONE
+    tlsAllowInvalidCertificates=True
 )
 
 db = client.expense_tracker
