5f1100bc2bfccd1351e7e0dfda938f12bc474f0a
Bauti Dallan <bautidallan2000@gmail.com>
2026-05-27 11:14:51 -0300

    Adding a Cluster

--- BackEnd/database.py
+++ BackEnd/database.py
@@ -1,6 +1,6 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-client = AsyncIOMotorClient("mongodb://localhost:27017")
+client = AsyncIOMotorClient("mongodb+srv://bautidallan2000_db_user:test@cluster0.wp9sbv0.mongodb.net/?appName=Cluster0")
 db = client.expense_app # one database
 
